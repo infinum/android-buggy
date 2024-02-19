0bea2388cb1b4c880e25c69a3b6cf2b167dc37b4
Antun Flaš <antun.flas@gmail.com>
2024-02-19 20:26:26 +0100

    Add buggy module

---
+++ buggy/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("java-library")
+    id("kotlin")
+}
+
+apply {
+    from("$rootDir/config.gradle.kts")
+    from("$rootDir/dokka.gradle")
+    from("$rootDir/maven-publish.gradle")
+    from("$rootDir/detekt.gradle")
+}
+
+val releaseConfig: Map<String, Any> by project
+val sonatype: Map<String, Any> by project
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+// specify per module - mostly needed due to different artifactIds, names, descriptions
+extra["mavenPublishProperties"] = mapOf(
+    "group" to releaseConfig["group"],
+    "version" to releaseConfig["version"],
+    "artifactId" to "buggy",
+    "repository" to mapOf(
+        "url" to sonatype["url"],
+        "username" to sonatype["username"],
+        "password" to sonatype["password"]
+    ),
+    "name" to "Buggy",
+    "description" to "Android library for collecting and exporting the application logs and debug data",
+    "url" to "https://github.com/infinum/android-buggy",
+    "scm" to mapOf(
+        "connection" to "https://github.com/infinum/android-buggy.git",
+        "url" to "https://github.com/infinum/android-buggy"
+    )
+)
