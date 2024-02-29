81d87d5f2f29ae64fb6ec90f09a5f6a749ca6d94
Karlo Čeh <karlo.ceh@infinum.com>
2024-02-29 16:00:00 +0100

    Add read latest logs to rolling logger screen

---
+++ sample/src/main/java/com/infinum/buggy/sample/logs/RollingLoggerAdapter.kt
@@ -0,0 +1,50 @@
+package com.infinum.buggy.sample.logs
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.infinum.buggy.sample.databinding.FragmentPlainExportBinding
+import com.infinum.buggy.sample.databinding.ItemLogBinding
+
+class RollingLoggerAdapter(
+
+) : ListAdapter<String, RollingLoggerViewHolder>(RollingLoggerDiffUtil()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RollingLoggerViewHolder =
+        RollingLoggerViewHolder(
+            ItemLogBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun onBindViewHolder(holder: RollingLoggerViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+}
+
+class RollingLoggerDiffUtil: DiffUtil.ItemCallback<String>() {
+    override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem === newItem
+    }
+
+    override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+
+}
+
+class RollingLoggerViewHolder(
+    private val binding: ItemLogBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(
+        log: String
+    ) {
+        binding.tvLog.text = log
+    }
+
+}
