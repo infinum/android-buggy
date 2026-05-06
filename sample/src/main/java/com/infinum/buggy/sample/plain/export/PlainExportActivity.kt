7304db9fce7637c0536eaac695520bbc0e02ccb9
Karlo Čeh <karlo.ceh@infinum.com>
2026-05-06 15:13:31 +0200

    Fix edge-to-edge support for sample app

--- sample/src/main/java/com/infinum/buggy/sample/plain/export/PlainExportActivity.kt
+++ sample/src/main/java/com/infinum/buggy/sample/plain/export/PlainExportActivity.kt
@@ -1,6 +1,7 @@
 package com.infinum.buggy.sample.plain.export
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.infinum.buggy.sample.databinding.ActivityPlainExportBinding
 
@@ -10,7 +11,7 @@ class PlainExportActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityPlainExportBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         viewBinding = ActivityPlainExportBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
     }
